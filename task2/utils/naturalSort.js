4e35a15acd16be550b78f736120512bae9a338a1
azamat <dinaev2002@gmail.com>
2023-08-03 17:56:25 +0300

    finish

---
+++ task2/utils/naturalSort.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.naturalSort = void 0;
+// src/utils/naturalSort.ts
+function naturalSort(a, b) {
+    return typeof a === "string" && typeof b === "string" ? a.localeCompare(b, undefined, { numeric: true }) : a - b;
+}
+exports.naturalSort = naturalSort;
